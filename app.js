ce235865466c18c7cd76ed79a5ea9083596c085a
sil-sg <sillvano.play.sg@gmail.com>
2026-03-12 15:20:00 +0100

    upgrade 1

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 import app from './api/index.js'
 import routes from './routes/index.js'
-import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const { PORT } = process.env
 
-app.use(cors())
-
-app.use('/api', routes)
-app.listen(PORT, () => console.log('Server is Running...'))
-
-// {
-//     origin: '*',
-//     methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-//     allowedHeaders: ['Content-Type','Authorization']
-// }
+export default app
