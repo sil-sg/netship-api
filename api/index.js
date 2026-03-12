82e20b2bdb5c7b786a9eff7f0bceb461f9b83718
Sílvio Gonçalves <silviogoncalves286@gmail.com>
2026-03-12 15:29:39 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import { start } from '../config/db.js'
+import routes from './routes/index.js'
 import cors from 'cors
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
+app.use(routes)
 app.use(cors())
 app.use(express.json())
 
 start()
 
-export default app
+const { PORT } = process.env
+
+app.listen(PORT, () => console.log('Server is Running...'))
