17ea51efbdda52745655620675392995bf2f1b8f
Sílvio Gonçalves <silviogoncalves286@gmail.com>
2026-03-12 15:25:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import app from './api/index.js'
 import routes from './routes/index.js'
 import dotenv from 'dotenv'
 dotenv.config()
-
+app.use(routes)
 const { PORT } = process.env
 
 app.listen(PORT, () => console.log('Server is Running...'))
